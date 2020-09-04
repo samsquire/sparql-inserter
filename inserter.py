9eec0160b965f94392e135f0ee70a057db6e2049
Samuel Michael Squire <sam@samsquire.com>
2020-09-04 16:51:00 +0200

    Sam example

--- inserter.py
+++ inserter.py
@@ -1,20 +1,31 @@
 import yaml
 from subprocess import Popen, PIPE
+from argparse import ArgumentParser
+
+
+parser = ArgumentParser()
+parser.add_argument("--file")
+args = parser.parse_args()
+inputfile = open(args.file, "r")
 
 def get_command(kind):
-    return ["bash", "-c", "curl -X POST -L http://localhost:9999/namespace/sam/sparql -H"Accept: text/csv" -H 'Content-Type: application/x-www-form-urlencoded; charset=UTF-8' -G --data-urlencode '" + kind + "@query.sparql'"]
+    return ["bash", "-c", "curl -s -X POST -L http://localhost:9999/namespace/sam/sparql -H'Accept: text/csv' -H 'Content-Type: application/x-www-form-urlencoded; charset=UTF-8' -G --data-urlencode '" + kind + "@query.sparql'"]
 
-for document in yaml.safe_load_all():
+for document in yaml.safe_load_all(inputfile):
+    if "comment" in document:
+        print(document["comment"])
+    kind = None
+
     if "query" in document:
-        f = open("query.sparql", "w")
-        f.write(document["query"])
-        command = get_command("query")
-
+        kind = "query"
     if "update" in document:
-        f = open("query.sparql", "w")
-        f.write(document["update"])
-        command = get_command("query")
+        kind = "update"
 
+    f = open("query.sparql", "w")
+    f.write(document[kind])
+    f.close()
+    command = get_command(kind)
     process = Popen(command, stdout=PIPE)
-    output = process.stdout.decode("utf-8").split("\n")
-    print(output)
+    output = process.stdout.read().decode("utf-8")
+    if kind == "query": 
+        print(output)
